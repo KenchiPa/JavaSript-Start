ce639d0366ea0f089dbdf0efa87169912ceed6e2
KenchiPa <kenchi6734@naver.com>
2023-01-28 21:27:45 +0900

    비교, 논리 연산자 추가

--- main.js
+++ main.js
@@ -1,59 +1,106 @@
-// 산술 연산자(arithmetic operator)
+// 논리 연산자(logical operator)
 
-console.log(1 + 2)  // 2
-console.log(5 - 7)  // -2
-console.log(3 * 4)  // 12 
-console.log(10 / 2)  // 5
-console.log(7 % 5)  // 나머지 연산자 // 2
+const a = 1 === 1
+const b = 'AB' === 'AB'
+const c = true
+const d = false
+const e = false
 
-// 할당 연산자(assignment operator)
+console.log(a)  // true
+console.log(b)  // true
+console.log(c)  // true
 
-let a = 2
-// a = a + 1  // 3
-// a += 1  // 3
-// a = a - 1  // 1 
-// a -= 1 // 1
-// a = a * 1  // 2 
-// a *= 1 // 2
-// a = a / 1  // 2
-// a /= 1 // 2
-console.log(a) 
+// 그리고 And 연산자
+console.log('&& :', a && b && c)  // true
+console.log('&& :', a && b && d)  // false
+// 또는 Or 연산자
+console.log('|| :', a || b)  // true
+console.log('|| :', e || d)  // false
+// 부정 Not 연산자
+console.log('! :', !d) // true
+
+// 비교 연산자(comparison operator)
+
+// const a = 1
+// const b = 1
+// const c = 2
+
+// // 일치 연산자
+// // console.log(a === b)  // true
+// // console.log(a === c)  // false
+// // console.log(a !== b)  // false
+// // console.log(a !== c)  // true
+// // console.log(a < b)  // false
+// // console.log(a < c)  // true
+// // console.log(a <= b)  // true
 
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 데이터 타입 확인
-// typeof: 데이터 타입 확인
+// // function isEqual(x, y){
+// //     return x === y
+// // }
 
-console.log('Hello World!');
-console.log(123);
-console.log(typeof 'Hello World!');
-console.log(typeof 123);
-console.log(typeof true);
-console.log(typeof undefined);
-console.log(typeof null);
-console.log(typeof {});
-console.log(typeof []);
+// // console.log(isEqual(1, 1))  // true
+// // console.log(isEqual(1, 2))  // false
+// // console.log(isEqual(2, '2'))  // false
 
-function getType(data){
-    return Object.prototype.toString.call(data)
-}
 
-console.log(getType(123))
-console.log(getType(false))
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // 산술 연산자(arithmetic operator)
 
-function getTypeSlice(data){
-    return Object.prototype.toString.call(data).slice(8) 
-}
+//  console.log(1 + 2)  // 2
+//  console.log(5 - 7)  // -2
+//  console.log(3 * 4)  // 12 
+//  console.log(10 / 2)  // 5
+//  console.log(7 % 5)  // 나머지 연산자 // 2
 
-console.log(getTypeSlice(123))
-console.log(getTypeSlice(false))
+// // 할당 연산자(assignment operator)
 
-function getTypeSlice2(data){
-    return Object.prototype.toString.call(data).slice(8, -1) 
-}
+// let h = 2
+// // h = h + 1  // 3
+// // h += 1  // 3
+// // h = h - 1  // 1 
+// // h -= 1 // 1
+// // h = h * 1  // 2 
+// // h *= 1 // 2
+// // h = h / 1  // 2
+// // h /= 1 // 2
+// console.log(h) 
 
-console.log(getTypeSlice2(123))
-console.log(getTypeSlice2(false))
-console.log(getTypeSlice2(null))
-console.log(getTypeSlice2({}))
-console.log(getTypeSlice2([]))
+
+// ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // 데이터 타입 확인
+// // typeof: 데이터 타입 확인
+
+// console.log('Hello World!');
+// console.log(123);
+// console.log(typeof 'Hello World!');
+// console.log(typeof 123);
+// console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof {});
+// console.log(typeof []);
+
+// function getType(data){
+//     return Object.prototype.toString.call(data)
+// }
+
+// console.log(getType(123))
+// console.log(getType(false))
+
+// function getTypeSlice(data){
+//     return Object.prototype.toString.call(data).slice(8) 
+// }
+
+// console.log(getTypeSlice(123))
+// console.log(getTypeSlice(false))
+
+// function getTypeSlice2(data){
+//     return Object.prototype.toString.call(data).slice(8, -1) 
+// }
+
+// console.log(getTypeSlice2(123))
+// console.log(getTypeSlice2(false))
+// console.log(getTypeSlice2(null))
+// console.log(getTypeSlice2({}))
+// console.log(getTypeSlice2([]))
